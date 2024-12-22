e5732793fe3fae8c2955ab485ce96141a21a64c0
Marko Milković <mmilkovic2006@gmail.com>
2024-12-23 00:30:02 +0100

    Updated comments section design a bit more

--- src/app/components/userComponent.tsx
+++ src/app/components/userComponent.tsx
@@ -8,7 +8,7 @@ const UserComponent = ({user}: {user: Profile}) => {
   return (
     <button className="hover:cursor-pointer flex gap-2 py-2 items-center" onClick={() => router.push(`/users/${user.username}`)}>
         <Flex gap="2">
-            <Avatar src={`${user.pictureUrl}`} style={{ width: '60px', height: '60px', borderRadius: '50%', boxShadow: '0px 3.08px 3.08px 0px #00000040'}} fallback="A" />
+            <Avatar src={`${user.pictureUrl}?${new Date().getTime()}`} style={{ width: '60px', height: '60px', borderRadius: '50%', boxShadow: '0px 3.08px 3.08px 0px #00000040'}} fallback="A" />
         </Flex>
         <div className="flex flex-col h-full items-start justify-center gap-3">
             <h1 className="text-black font-[400] font-Roboto leading-[8.23px] text-lg">{user.firstName} {user.lastName}</h1>
