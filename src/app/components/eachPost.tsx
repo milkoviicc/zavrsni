252e1b8147d83be5c8c5086bc9ff31cceb11d848
Marko Milković <mmilkovic2006@gmail.com>
2025-01-20 00:52:31 +0100

    Liking and disliking still bugging

--- src/app/components/eachPost.tsx
+++ src/app/components/eachPost.tsx
@@ -54,9 +54,9 @@ const EachPost = ({post, handleLike, handleDislike, deletePost, updatePost, refr
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [updatedFiles, setUpdatedFiles] = useState<File[]>([]);
   const [previousFiles, setPreviousFiles] = useState<string[]>([]);
-  const [postReaction, setPostReaction] = useState<number>(post.userReacted);
-  const [postLikes, setPostLikes] = useState(post.likes);
-  const [postDislikes, setPostDislikes] = useState(post.dislikes);
+  const [postReaction, setPostReaction] = useState<number>(0);
+  const [postLikes, setPostLikes] = useState(0);
+  const [postDislikes, setPostDislikes] = useState(0);
   const [isProcessing, setIsProcessing] = useState(false);
 
   // dobivam usera iz localStorage-a
@@ -107,8 +107,6 @@ const EachPost = ({post, handleLike, handleDislike, deletePost, updatePost, refr
       console.error(err);
     }
   }
-  
-  const feed = localStorage.getItem('feed');
 
   useEffect(() => {
 
@@ -132,6 +130,12 @@ const EachPost = ({post, handleLike, handleDislike, deletePost, updatePost, refr
     }
 
   }, [user, post.user.userId, post]);  
+
+  useEffect(() => {
+    setPostLikes(post.likes);
+    setPostDislikes(post.dislikes);
+    setPostReaction(post.userReacted);
+  }, [post.userReacted, post.likes, post.dislikes])
 
 
   const handleComments = async () => {
@@ -165,52 +169,50 @@ const handleReaction = async (reaction: number) => {
   setTajmout(undefined);
   try {
     // Handle reactions based on current state
-    if (isLiked === 1 && isDisliked === 0 && reaction === 1) {
+    if (postReaction === 1 && reaction === 1) {
       // Undo a like
-      setIsLiked(0);
+      setPostReaction(0);
       setPostLikes((prev) => prev - 1);
       setTajmout(setTimeout(async () => {
         await handleLike(post.postId);
-      }, 500));
-    } else if (isLiked === 0 && isDisliked === 0 && reaction === 1) {
+      }, 750));
+    } else if (postReaction === 0 && reaction === 1) {
       // Add a like
-      setIsLiked(1);
+      setPostReaction(1);
       setPostLikes((prev) => prev + 1);
       setTajmout(setTimeout(async () => {
         await handleLike(post.postId); // Backend call
-      }, 500));
-    } else if (isLiked === 1 && isDisliked === 0 && reaction === -1) {
+      }, 750));
+    } else if (postReaction === 1 && reaction === -1) {
       // Switch from like to dislike
-      setIsLiked(0);
-      setIsDisliked(-1);
+      setPostReaction(-1)
       setPostLikes((prev) => prev - 1);
       setPostDislikes((prev) => prev + 1);
       setTajmout(setTimeout(async () => {
         await handleDislike(post.postId); // Backend call
-      }, 500));
-    } else if (isDisliked === 0 && isDisliked === 0 && reaction === -1) {
+      }, 750));
+    } else if (postReaction === 0 && reaction === -1) {
       // Add a dislike
-      setIsDisliked(-1);
+      setPostReaction(-1);
       setPostDislikes((prev) => prev + 1);
       setTajmout(setTimeout(async () => {
         await handleDislike(post.postId); // Backend call
-      }, 500));
-    } else if (isDisliked === -1 && isLiked === 0 && reaction === -1) {
+      }, 750));
+    } else if (postReaction === -1 && reaction === -1) {
       // Undo a dislike
-      setIsDisliked(0);
+      setPostReaction(0);
       setPostDislikes((prev) => prev - 1);
       setTajmout(setTimeout(async () => {
         await handleDislike(post.postId); // Backend call
-      }, 500));
-    } else if (isDisliked === -1 && isLiked === 0 && reaction === 1) {
+      }, 750));
+    } else if (postReaction === -1 && reaction === 1) {
       // Switch from dislike to like
-      setIsDisliked(0);
-      setIsLiked(1);
+      setPostReaction(1);
       setPostLikes((prev) => prev + 1);
       setPostDislikes((prev) => prev - 1);
       setTajmout(setTimeout(async () => {
         await handleLike(post.postId); // Backend call
-      }, 500));
+      }, 750));
     }
   } catch (err) {
     console.error("Error handling reaction:", err);
@@ -332,10 +334,10 @@ const handleReaction = async (reaction: number) => {
           <div className="flex gap-4 pt-4 pb-0 items-center justify-between">
             <div className="flex justify-between w-full">
               <div className='flex gap-1 px-4 items-center'>
-                <button onClick={() => handleReaction(1)}><svg width="20" height="20" viewBox="0 0 7 7" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_57_98)"><path d="M0.0175432 3.20833L2.87879 0.259583C3.04796 0.0904165 3.26963 -1.62297e-07 3.50296 -1.52097e-07C3.73629 -1.41898e-07 3.95796 0.0904165 4.12129 0.256667L6.98254 3.20833L4.96129 3.20833L4.96129 7L2.04463 7L2.04463 3.20833L0.0175432 3.20833Z" fill={`${isLiked === 1  ? '#319357' : '#585858'}`}/></g><defs><clipPath id="clip0_57_98"><rect width="7" height="7" fill="white" transform="translate(7) rotate(90)"/></clipPath></defs></svg></button>
-                <p className={`${isLiked === 1  ? 'text-[#319357]' : 'text-[#585858]'}`}>{postLikes}</p>
-                <button onClick={() => handleReaction(-1)}><svg width="20" height="20" viewBox="0 0 7 7" xmlns="http://www.w3.org/2000/svg"><g id="Layer_1" clipPath="url(#clip0_57_85)"><path id="Vector" d="M6.98246 3.79167L4.12121 6.74042C3.95204 6.90958 3.73037 7 3.49704 7C3.26371 7 3.04204 6.90958 2.87871 6.74333L0.0174562 3.79167L2.03871 3.79167L2.03871 -3.88486e-07L4.95537 -2.60994e-07L4.95537 3.79167L6.98246 3.79167Z" fill={`${isDisliked === -1  ? '#D25551' : '#585858'}`}/></g><defs><clipPath id="clip0_57_85"><rect width="7" height="7" fill="white" transform="translate(0 7) rotate(-90)"/></clipPath></defs></svg></button>
-                <p className={`${isDisliked === -1  ? 'text-[#D25551]' : 'text-[#585858]'}`}>{postDislikes}</p>
+                <button onClick={() => handleReaction(1)}><svg width="20" height="20" viewBox="0 0 7 7" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_57_98)"><path d="M0.0175432 3.20833L2.87879 0.259583C3.04796 0.0904165 3.26963 -1.62297e-07 3.50296 -1.52097e-07C3.73629 -1.41898e-07 3.95796 0.0904165 4.12129 0.256667L6.98254 3.20833L4.96129 3.20833L4.96129 7L2.04463 7L2.04463 3.20833L0.0175432 3.20833Z" fill={`${postReaction === 1  ? '#319357' : '#585858'}`}/></g><defs><clipPath id="clip0_57_98"><rect width="7" height="7" fill="white" transform="translate(7) rotate(90)"/></clipPath></defs></svg></button>
+                <p className={`${postReaction === 1  ? 'text-[#319357]' : 'text-[#585858]'}`}>{postLikes}</p>
+                <button onClick={() => handleReaction(-1)}><svg width="20" height="20" viewBox="0 0 7 7" xmlns="http://www.w3.org/2000/svg"><g id="Layer_1" clipPath="url(#clip0_57_85)"><path id="Vector" d="M6.98246 3.79167L4.12121 6.74042C3.95204 6.90958 3.73037 7 3.49704 7C3.26371 7 3.04204 6.90958 2.87871 6.74333L0.0174562 3.79167L2.03871 3.79167L2.03871 -3.88486e-07L4.95537 -2.60994e-07L4.95537 3.79167L6.98246 3.79167Z" fill={`${postReaction === -1  ? '#D25551' : '#585858'}`}/></g><defs><clipPath id="clip0_57_85"><rect width="7" height="7" fill="white" transform="translate(0 7) rotate(-90)"/></clipPath></defs></svg></button>
+                <p className={`${postReaction === -1  ? 'text-[#D25551]' : 'text-[#585858]'}`}>{postDislikes}</p>
               </div>
               <Dialog>
                 <DialogTrigger className='flex px-8 py-2 rounded-md w-fit text-[#545454]' onClick={() => handleComments()}><svg className='mr-1' width="24" height="24" viewBox="0 0 8 8" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_54_1461)"> <path d="M7.34927 2.1044C7.12027 1.42273 6.57694 0.879395 5.89561 0.650729C4.63761 0.228729 3.36194 0.228729 2.10427 0.650729C1.42261 0.879395 0.879273 1.42273 0.650606 2.10406C0.228606 3.36206 0.228606 4.6374 0.650606 5.8954C0.879273 6.57673 1.42261 7.1204 2.10427 7.34906C2.73227 7.55973 3.36994 7.6664 3.99994 7.6664C5.00727 7.6664 6.02794 7.53373 7.03361 7.27206C7.15061 7.24173 7.24194 7.1504 7.27227 7.0334C7.53394 6.02773 7.66661 5.00706 7.66661 3.99973C7.66661 3.37006 7.55994 2.7324 7.34927 2.10406V2.1044ZM6.67527 6.67506C5.78627 6.89073 4.88727 7.00006 4.00027 7.00006C3.44294 7.00006 2.87627 6.90473 2.31694 6.71706C1.83227 6.55439 1.44594 6.16806 1.28327 5.68339C0.908273 4.56639 0.908273 3.4334 1.28327 2.3164C1.44561 1.83173 1.83194 1.4454 2.31694 1.28306C2.87527 1.09573 3.43794 1.00173 4.00027 1.00173C4.56261 1.00173 5.12527 1.0954 5.68394 1.28306C6.16861 1.4454 6.55494 1.83173 6.71761 2.31673C6.90527 2.8764 7.00061 3.44273 7.00061 4.00006C7.00061 4.8874 6.89127 5.78606 6.67561 6.67506H6.67527Z" fill="#545454"/> <path d="M5.68797 3.66675H2.31197C2.12797 3.66675 1.97864 3.81608 1.97864 4.00008C1.97864 4.18408 2.12797 4.33341 2.31197 4.33341H5.68797C5.87197 4.33341 6.0213 4.18408 6.0213 4.00008C6.0213 3.81608 5.87197 3.66675 5.68797 3.66675Z" fill="#545454"/> <path d="M5.35468 5.00708H2.64535C2.46135 5.00708 2.31201 5.15641 2.31201 5.34041C2.31201 5.52441 2.46135 5.67375 2.64535 5.67375H5.35468C5.53868 5.67375 5.68801 5.52441 5.68801 5.34041C5.68801 5.15641 5.53868 5.00708 5.35468 5.00708Z" fill="#545454"/> <path d="M2.64535 2.99308H3.61335C3.79735 2.99308 3.94668 2.84375 3.94668 2.65975C3.94668 2.47575 3.79735 2.32642 3.61335 2.32642H2.64535C2.46135 2.32642 2.31201 2.47575 2.31201 2.65975C2.31201 2.84375 2.46135 2.99308 2.64535 2.99308Z" fill="#545454"/> </g> <defs> <clipPath id="clip0_54_1461"> <rect width="8" height="8" fill="white"/></clipPath></defs></svg>{post.commentCount}</DialogTrigger>
@@ -391,10 +393,10 @@ const handleReaction = async (reaction: number) => {
                           />))}
                     </div>
                     <div className='flex gap-2 w-[95%] pt-4 pb-0'>
-                      <button onClick={() => handleReaction(1)} disabled={isProcessing}><svg width="20" height="20" viewBox="0 0 7 7" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_57_98)"><path d="M0.0175432 3.20833L2.87879 0.259583C3.04796 0.0904165 3.26963 -1.62297e-07 3.50296 -1.52097e-07C3.73629 -1.41898e-07 3.95796 0.0904165 4.12129 0.256667L6.98254 3.20833L4.96129 3.20833L4.96129 7L2.04463 7L2.04463 3.20833L0.0175432 3.20833Z" fill={`${isLiked === 1 ? '#319357' : '#585858'}`}/></g><defs><clipPath id="clip0_57_98"><rect width="7" height="7" fill="white" transform="translate(7) rotate(90)"/></clipPath></defs></svg></button>
-                      <p className={`${isLiked === 1  ? 'text-[#319357]' : 'text-[#585858]'}`}>{postLikes}</p>
-                      <button onClick={() => handleReaction(-1)} disabled={isProcessing}><svg width="20" height="20" viewBox="0 0 7 7" xmlns="http://www.w3.org/2000/svg"><g id="Layer_1" clipPath="url(#clip0_57_85)"><path id="Vector" d="M6.98246 3.79167L4.12121 6.74042C3.95204 6.90958 3.73037 7 3.49704 7C3.26371 7 3.04204 6.90958 2.87871 6.74333L0.0174562 3.79167L2.03871 3.79167L2.03871 -3.88486e-07L4.95537 -2.60994e-07L4.95537 3.79167L6.98246 3.79167Z" fill={`${isDisliked === -1  ? '#D25551' : '#585858'}`}/></g><defs><clipPath id="clip0_57_85"><rect width="7" height="7" fill="white" transform="translate(0 7) rotate(-90)"/></clipPath></defs></svg></button>
-                      <p className={`${isDisliked === -1  ? 'text-[#D25551]' : 'text-[#585858]'}`}>{postDislikes}</p>
+                      <button onClick={() => handleReaction(1)} disabled={isProcessing}><svg width="20" height="20" viewBox="0 0 7 7" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_57_98)"><path d="M0.0175432 3.20833L2.87879 0.259583C3.04796 0.0904165 3.26963 -1.62297e-07 3.50296 -1.52097e-07C3.73629 -1.41898e-07 3.95796 0.0904165 4.12129 0.256667L6.98254 3.20833L4.96129 3.20833L4.96129 7L2.04463 7L2.04463 3.20833L0.0175432 3.20833Z" fill={`${postReaction === 1 ? '#319357' : '#585858'}`}/></g><defs><clipPath id="clip0_57_98"><rect width="7" height="7" fill="white" transform="translate(7) rotate(90)"/></clipPath></defs></svg></button>
+                      <p className={`${postReaction === 1  ? 'text-[#319357]' : 'text-[#585858]'}`}>{postLikes}</p>
+                      <button onClick={() => handleReaction(-1)} disabled={isProcessing}><svg width="20" height="20" viewBox="0 0 7 7" xmlns="http://www.w3.org/2000/svg"><g id="Layer_1" clipPath="url(#clip0_57_85)"><path id="Vector" d="M6.98246 3.79167L4.12121 6.74042C3.95204 6.90958 3.73037 7 3.49704 7C3.26371 7 3.04204 6.90958 2.87871 6.74333L0.0174562 3.79167L2.03871 3.79167L2.03871 -3.88486e-07L4.95537 -2.60994e-07L4.95537 3.79167L6.98246 3.79167Z" fill={`${postReaction === -1  ? '#D25551' : '#585858'}`}/></g><defs><clipPath id="clip0_57_85"><rect width="7" height="7" fill="white" transform="translate(0 7) rotate(-90)"/></clipPath></defs></svg></button>
+                      <p className={`${postReaction === -1  ? 'text-[#D25551]' : 'text-[#585858]'}`}>{postDislikes}</p>
                     </div>
                     <hr className='my-4 w-[97%] h-[1px] bg-black'/>
                     <PostComment post={post} refreshPosts={refreshPosts} refreshComments={handleComments} setComments={setComments}/>
