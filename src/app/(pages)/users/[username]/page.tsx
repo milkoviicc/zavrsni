392c764066070049e0fbea9542302a0a96f562e9
Marko Milković <mmilkovic2006@gmail.com>
2025-02-17 01:31:01 +0100

    Fixed user's friends design

--- src/app/(pages)/users/[username]/page.tsx
+++ src/app/(pages)/users/[username]/page.tsx
@@ -181,18 +181,18 @@ const UserProfile = () => {
               <div className='flex justify-center w-screen px-4'>
                 <div className={`xl:hidden group max-w-[400px] sm:max-w-[570px] md:max-w-[716px] lg:max-w-[765px] w-full h-full flex flex-col items-center gap-2 bg-transparent px-2 rounded-lg shadow-[0px_0.1px_15px_0px_rgba(0,_0,_0_,_0.26)] py-4 mb-12`}>
                   <h1 className='font-Roboto text-[#DFDEDE] text-2xl text-center'>{myProfile ? 'Your friends' : `${user.firstName}'s friends`}</h1>
-                  <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6'>
+                  <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4'>
                     {getFriendsQuery.isLoading || isRendering ? <UserSkeleton /> : getFriendsQuery.data?.map((user, index, array) => {
                       const itemsInLastRow = array.length % columns; // Defaulting to 4 when possible
                       const isLastOdd = itemsInLastRow !== 0 && index >= array.length - itemsInLastRow;
                       return (
-                      <div key={user.user.userId} className={`hover:cursor-pointer flex gap-2 py-2 items-center ${isLastOdd && itemsInLastRow === 1 && getFriendsQuery.data?.length !== 1 ? 'col-span-full justify-center w-full' : ' w-[170px]'}`} onClick={() => router.push(`/users/${user.user.username}`)}>
+                      <div key={user.user.userId} className={`hover:cursor-pointer flex py-2 gap-2 ${isLastOdd && itemsInLastRow === 1 && getFriendsQuery.data?.length !== 1 ? 'col-span-full justify-center w-full' : 'w-fit'}`} onClick={() => router.push(`/users/${user.user.username}`)}>
                         <Avatar className='w-[35px] h-[35px] 2xl:w-[55px] 2xl:h-[55px] 2k:w-[65px] 2k:h-[65px] rounded-full'>
                             <AvatarImage src={`${user.user.pictureUrl}`} className="w-fit h-fit aspect-square rounded-full object-cover" style={{boxShadow: '0px 3.08px 3.08px 0px #00000040'}} /><AvatarFallback>{user.user.username.slice(0, 1)}</AvatarFallback>
                         </Avatar>
                         <div className="flex flex-col h-full items-start justify-center">
-                            <h1 className={`text-[#EFEFEF] font-[400] font-Roboto text-sm sm:text-base 2k:text-lg truncate whitespace-nowrap ${isLastOdd && itemsInLastRow === 1 ? 'max-w-full' : 'max-w-[85px] sm:max-w-[100px] lg:max-w-full'}`} title={`${user.user.firstName} ${user.user.lastName}`}>{user.user.firstName} {user.user.lastName}</h1>
-                            <p className={`text-[#888888]  text-sm sm:text-base ${isLastOdd && itemsInLastRow === 1 ? 'max-w-full' : 'max-w-[75px] sm:max-w-[100px] lg:max-w-full'} 2k:text-lg truncate whitespace-nowrap`}>@{user.user.username}</p>
+                            <h1 className={`text-[#EFEFEF] font-[400] font-Roboto text-sm sm:text-base 2k:text-lg truncate whitespace-nowrap max-w-full`} title={`${user.user.firstName} ${user.user.lastName}`}>{user.user.firstName} {user.user.lastName}</h1>
+                            <p className={`text-[#888888]  text-sm sm:text-base ${isLastOdd && itemsInLastRow === 1 ? 'max-w-full' : 'max-w-[100px]'} 2k:text-lg truncate whitespace-nowrap`}>@{user.user.username}</p>
                         </div>    
                       </div>
                     )})}
