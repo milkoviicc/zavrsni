a8da5eb6aa2d2088e44c6115c511f085169d2b3a
Marko <82616233+milkoviicc@users.noreply.github.com>
2025-02-22 16:17:04 +0100

    Updated Footer.tsx

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -8,9 +8,9 @@ export default function Footer() {
 
     return (
         <div className="bg-[#222222] h-[80px] w-full shadow-[0px_-0.5px_20.16px_0px_rgba(0,0,0,0.26)] z-[100]">
-            <div className="flex justify-evenly items-center h-full">
-                <p className="text-sm font-bold flex items-center text-[#D0D0D0]">milkoviicc <span className="mt-1 px-2">©</span> 2024</p>
-                <div className="flex gap-4"> 
+            <div className="flex justify-evenly items-center h-full text-center">
+                <p className="text-center text-sm font-bold font-Roboto text-[#D0D0D0] px-2 w-full">Developed by Milkovic & Novosel &nbsp;&nbsp;&nbsp;&nbsp;© 2024</p>
+                <div className="flex gap-4 w-full justify-center"> 
                     <a href="https://instagram.com" className="text-2xl hover:text-blue-800 text-[#D0D0D0] transition-all"><FontAwesomeIcon icon={faInstagram} /></a>
                     <a href="https://x.com" className="text-2xl hover:text-blue-800 text-[#D0D0D0] transition-all"><FontAwesomeIcon icon={faTwitter} /></a>
                     <a href="https://linkedin.com" className="text-2xl hover:text-blue-800 text-[#D0D0D0] transition-all"><FontAwesomeIcon icon={faLinkedin} /></a>
