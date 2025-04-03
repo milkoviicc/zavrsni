5cbb8d7e338dabbdf9ef7e4ad66b2295dec29798
Marko <82616233+milkoviicc@users.noreply.github.com>
2025-04-03 16:48:32 +0200

    Updated you might know users

--- src/components/profile/UserDetails.tsx
+++ src/components/profile/UserDetails.tsx
@@ -5,33 +5,41 @@ import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { FriendshipStatus, Profile, User } from '@/src/types/types';
 import { followsApi, friendsApi } from '@/src/lib/utils';
 import { useRouter } from 'next/navigation';
+import { useAuth } from '@/src/context/AuthProvider';
 
-const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile, friendship: FriendshipStatus | null, mutualFriends: User[] | null, popular: User[]}) => {
+const UserDetails = ({pathUser, friendship, mutualFriends, popular}: {pathUser: Profile, friendship: FriendshipStatus | null, mutualFriends: User[] | null, popular: User[]}) => {
 
   const [shortUsername, setShortUsername] = useState('');
   const [friendshipStatus, setFriendshipStatus] = useState(0);
   const [isFollowed, setIsFollowed] = useState(false);
-
+  const [popularUsers, setPopularUsers] = useState<User[]>([]);
+  const {user} = useAuth();
   const router = useRouter();
 
   useEffect(() => {
-    if(user && user.firstName && user.lastName) {
-      setShortUsername(user.firstName.slice(0,1) + user.lastName.slice(0,1));
+    if(pathUser && pathUser.firstName && pathUser.lastName) {
+      setShortUsername(pathUser.firstName.slice(0,1) + pathUser.lastName.slice(0,1));
     }
 
     if(friendship) {
       setFriendshipStatus(friendship.friendshipStatus);
       setIsFollowed(friendship.isFollowed);
     }
-  }, [user, friendship]);
+
+    if(popular && user) {
+      const fillUsers = popular.filter(popularUser => popularUser.userId !== pathUser.userId);
+      const newPopularUsers = fillUsers.filter(filledUser => filledUser.userId !== user.userId).slice(0,4);
+      setPopularUsers(newPopularUsers);
+    }
+  }, [pathUser, friendship, popular]);
 
   const handleFollow = async (id: string) => {
     if(isFollowed) {
       setIsFollowed(false);
-      user.followers--;
+      pathUser.followers--;
     } else {
       setIsFollowed(true);
-      user.followers++;
+      pathUser.followers++;
     }
     try {
         if(isFollowed) {
@@ -48,7 +56,7 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
 
     setFriendshipStatus(3);
     try {
-      const res = await friendsApi.acceptFriendRequest(user.userId);
+      const res = await friendsApi.acceptFriendRequest(pathUser.userId);
     } catch(err) {
       console.error(err);
     }
@@ -57,7 +65,7 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
   const declineRequest = async () => {
       setFriendshipStatus(0);
       try {
-        const res = await friendsApi.declineFriendRequest(user.userId);
+        const res = await friendsApi.declineFriendRequest(pathUser.userId);
       } catch(err) {
           console.error(err);
       }
@@ -66,7 +74,7 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
   const addFriend = async () => {
     setFriendshipStatus(1);
     try {
-      const res = await friendsApi.sendFriendRequest(user.userId);
+      const res = await friendsApi.sendFriendRequest(pathUser.userId);
     } catch(err) {
       console.error(err);
     }
@@ -75,7 +83,7 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
   const unfriend = async () => {
     setFriendshipStatus(0);
     try {
-      const res = await friendsApi.deleteFriend(user.userId);
+      const res = await friendsApi.deleteFriend(pathUser.userId);
     } catch(err) {
       console.error(err);
     }
@@ -84,7 +92,7 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
   const unsendFriendReq = async () => {
     setFriendshipStatus(0);
     try {
-      const res = await friendsApi.declineFriendRequest(user.userId);
+      const res = await friendsApi.declineFriendRequest(pathUser.userId);
     } catch(err) {
       console.error(err);
     }
@@ -97,28 +105,28 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
           <div className='w-[350px] sm:w-[580px] md:w-[716px] lg:w-[765px] xl:hidden flex flex-col justify-center items-center gap-2 px-2 lg:px-8 py-4 rounded-lg shadow-[0px_0.1px_15px_0px_rgba(0,_0,_0_,_0.26)]'>
             <div className='flex items-center justify-center gap-1'>
               <Avatar className='w-[125px] h-[125px] relative shrink-0 overflow-x-hidden rounded-full shadow-[0px_5px_5px_0px_rgba(0,_0,_0_,_0.25)]'>
-                <AvatarImage src={`${user?.pictureUrl}`} className="w-fit h-fit aspect-square rounded-full object-cover" /><AvatarFallback>{shortUsername}</AvatarFallback>
+                <AvatarImage src={`${pathUser?.pictureUrl}`} className="w-fit h-fit aspect-square rounded-full object-cover" /><AvatarFallback>{shortUsername}</AvatarFallback>
               </Avatar>
               <div className='flex flex-col justify-center pl-4'>
-                <h1 className='text-[#DFDEDE] font-Roboto text-2xl'>{user.firstName} {user.lastName}</h1>
-                <p className='text-[#888888] font-Roboto text-xl'>@{user.username}</p>
+                <h1 className='text-[#DFDEDE] font-Roboto text-2xl'>{pathUser.firstName} {pathUser.lastName}</h1>
+                <p className='text-[#888888] font-Roboto text-xl'>@{pathUser.username}</p>
               </div>
             </div>
             <div className='flex flex-col w-full'>
               <div className='flex flex-col gap-2 justify-center'>
                 <div className='flex flex-col items-center px-4 py-2 gap-4'>
-                  <p className='text-center break-words hyphens-auto text-[#888888] font-Roboto text-sm w-[80%]'>{user.description ? `${user.description}` : 'No description yet! You can add one down below.'}</p>
+                  <p className='text-center break-words hyphens-auto text-[#888888] font-Roboto text-sm w-[80%]'>{pathUser.description ? `${pathUser.description}` : 'No description yet! You can add one down below.'}</p>
                 </div>
                 <div className='flex items-center justify-evenly gap-4'>
-                  <p className='text-[#888888] text-center font-Roboto text-sm'>{user.occupation ? `${user.occupation}` : 'No occupation yet!'}</p>
+                  <p className='text-[#888888] text-center font-Roboto text-sm'>{pathUser.occupation ? `${pathUser.occupation}` : 'No occupation yet!'}</p>
                   <div className='flex gap-4'>
                     <div className='flex items-center gap-2'>
                       <p className='text-[#888888] font-Roboto text-xs 2xl:text-sm'>Followers</p>
-                      <span className='text-[#888888] text-lg'>{user.followers}</span>
+                      <span className='text-[#888888] text-lg'>{pathUser.followers}</span>
                     </div>
                     <div className='flex items-center gap-2'>
                       <p className='text-[#888888] font-Roboto text-xs 2xl:text-sm'>Following</p>
-                      <span className='text-[#888888] text-lg'>{user.following}</span>
+                      <span className='text-[#888888] text-lg'>{pathUser.following}</span>
                     </div>
                   </div>
                 </div>
@@ -150,8 +158,8 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
                 )}
               </div>
               <div className='w-full flex flex-col justify-center items-center gap-2'>
-                <p className={`font-Roboto text-[#808080] text-[11px] sm:text-sm `}>{isFollowed ? `You are following ` : `You're not following `}{user.firstName}</p>
-                <button className={`${isFollowed ? 'bg-[#CA3C3C] shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]' : 'bg-[#1565CE] shadow-[1px_1px_3px_0px_rgba(12,75,156,1)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)]'} px-4 sm:px-4 py-1 w-fit h-fit rounded-2xl font-Roboto text-[#E3E3E3] text-sm transition-all hover:opacity-90 cursor-pointer`} onClick={() => handleFollow(user.userId)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
+                <p className={`font-Roboto text-[#808080] text-[11px] sm:text-sm `}>{isFollowed ? `You are following ` : `You're not following `}{pathUser.firstName}</p>
+                <button className={`${isFollowed ? 'bg-[#CA3C3C] shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]' : 'bg-[#1565CE] shadow-[1px_1px_3px_0px_rgba(12,75,156,1)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)]'} px-4 sm:px-4 py-1 w-fit h-fit rounded-2xl font-Roboto text-[#E3E3E3] text-sm transition-all hover:opacity-90 cursor-pointer`} onClick={() => handleFollow(pathUser.userId)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
               </div>
             </div>
             <span className='bg-[#515151] h-[1px] w-full mt-2'></span>
@@ -169,7 +177,7 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
                     </div>    
                   </div>
                 )) : 
-                popular?.map((profile, index, array) => {
+                popularUsers?.map((profile, index, array) => {
                   const isLastOdd = array.length % 2 !== 0 && index === array.length -1;
                   return (
                     <div key={profile.userId} className={`hover:cursor-pointer flex gap-2 py-2 items-center ${isLastOdd ? "col-span-2 justify-center" : "w-fit"}`} onClick={() => router.push(`/users/${profile.username}`)}>
@@ -192,25 +200,25 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
         <div className='flex flex-col py-1 gap-2 w-full'>
           <div className='flex px-3'>
             <Avatar className='w-[45px] h-[45px] 2xl:w-[65px] 2xl:h-[65px] 2k:w-[100px] 2k:h-[100px] rounded-full shadow-[0px_5px_5px_0px_rgba(0,_0,_0_,_0.25)]'>
-              <AvatarImage src={`${user?.pictureUrl}`} className="w-fit h-fit aspect-square rounded-full object-cover"  /><AvatarFallback>{shortUsername}</AvatarFallback>
+              <AvatarImage src={`${pathUser?.pictureUrl}`} className="w-fit h-fit aspect-square rounded-full object-cover"  /><AvatarFallback>{shortUsername}</AvatarFallback>
             </Avatar>
             <div className='flex flex-col justify-center items-start px-2 2k:px-4'>
-              <h1 className='text-[#DFDEDE] font-Roboto text-sm 2xl:text-base 2k:text-lg'>{user.firstName} {user.lastName}</h1>
-              <p className='text-[#888888] font-Roboto text-sm 2xl:text-base 2k:text-lg'>@{user.username}</p>
+              <h1 className='text-[#DFDEDE] font-Roboto text-sm 2xl:text-base 2k:text-lg'>{pathUser.firstName} {pathUser.lastName}</h1>
+              <p className='text-[#888888] font-Roboto text-sm 2xl:text-base 2k:text-lg'>@{pathUser.username}</p>
             </div>
           </div>
           <div className='flex flex-col px-3 py-2 gap-4'>
-            <p className='text-left break-words hyphens-auto text-[#888888] px-[5px] font-Roboto text-xs 2xl:text-sm 2k:text-base'>{user.description ? `${user.description}` : 'No description yet! You can add one down below.'}</p>
-            <p className='text-[#888888] text-center font-Roboto text-xs 2xl:text-sm 2k:text-base'>{user.occupation ? `${user.occupation}` : 'No occupation yet!'}</p>
+            <p className='text-left break-words hyphens-auto text-[#888888] px-[5px] font-Roboto text-xs 2xl:text-sm 2k:text-base'>{pathUser.description ? `${pathUser.description}` : 'No description yet! You can add one down below.'}</p>
+            <p className='text-[#888888] text-center font-Roboto text-xs 2xl:text-sm 2k:text-base'>{pathUser.occupation ? `${pathUser.occupation}` : 'No occupation yet!'}</p>
           </div>
           <div className='flex justify-evenly gap-4'>
             <div className='flex items-center gap-2'>
             <p className='text-[#888888] font-Roboto text-xs 2xl:text-sm'>Followers</p>
-            <span className='text-[#888888] text-lg'>{user.followers}</span>
+            <span className='text-[#888888] text-lg'>{pathUser.followers}</span>
             </div>
             <div className='flex items-center gap-2'>
             <p className='text-[#888888] font-Roboto text-xs 2xl:text-sm'>Following</p>
-            <span className='text-[#888888] text-lg'>{user.following}</span>
+            <span className='text-[#888888] text-lg'>{pathUser.following}</span>
             </div>
           </div>
           <span className="bg-[#515151] h-[1px] w-full"></span>
@@ -220,12 +228,12 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
           {friendshipStatus === 0 ? (
             <div className='w-full px-2 flex justify-between gap-4 pt-2 text-sm 2xl:text-base'>
               <button onClick={() => addFriend()} className='px-2 py-0 w-full rounded-full font-Roboto font-normal bg-[#1565CE] transition-all shadow-[0px_1px_2px_0px_rgba(110, 122, 248, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)] hover:opacity-90 text-[#E3E3E3] cursor-pointer'>Add friend</button>
-              <button className={`${isFollowed ? 'bg-[#CA3C3C] shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]' : 'bg-[#1565CE] shadow-[1px_1px_3px_0px_rgba(12,75,156,1)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)]'} px-2 sm:px-4 w-full h-fit rounded-2xl font-Roboto text-[#E3E3E3] transition-all  hover:opacity-90 cursor-pointer`} onClick={() => handleFollow(user.userId)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
+              <button className={`${isFollowed ? 'bg-[#CA3C3C] shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]' : 'bg-[#1565CE] shadow-[1px_1px_3px_0px_rgba(12,75,156,1)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)]'} px-2 sm:px-4 w-full h-fit rounded-2xl font-Roboto text-[#E3E3E3] transition-all  hover:opacity-90 cursor-pointer`} onClick={() => handleFollow(pathUser.userId)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
             </div>
           ) : friendshipStatus === 1 ? (
             <div className='w-full px-2 flex justify-between gap-4 pt-2'>
               <button onClick={() => unsendFriendReq()} className='px-2 py-0 w-full h-full rounded-full font-Roboto font-normal bg-[#CA3C3C] transition-all shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(0,0,0,0.2)] hover:opacity-90 text-[#E3E3E3] cursor-pointer'>Unsend</button>
-              <button className={`${isFollowed ? 'bg-[#CA3C3C] shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]' : 'bg-[#1565CE] shadow-[1px_1px_3px_0px_rgba(12,75,156,1)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)]'} px-2 sm:px-4 w-full h-fit rounded-2xl font-Roboto text-[#E3E3E3] transition-all  hover:opacity-90 cursor-pointer`} onClick={() => handleFollow(user.userId)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
+              <button className={`${isFollowed ? 'bg-[#CA3C3C] shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]' : 'bg-[#1565CE] shadow-[1px_1px_3px_0px_rgba(12,75,156,1)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)]'} px-2 sm:px-4 w-full h-fit rounded-2xl font-Roboto text-[#E3E3E3] transition-all  hover:opacity-90 cursor-pointer`} onClick={() => handleFollow(pathUser.userId)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
             </div>
           ) : friendshipStatus === 2 ? (
             <div className='w-full px-2 flex flex-col gap-4 pt-2'>
@@ -234,13 +242,13 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
                 <button onClick={() => declineRequest()} className='px-2 py-0 w-full h-fit rounded-full font-Roboto font-normal bg-[#CA3C3C] transition-all shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]  hover:opacity-90 text-[#E3E3E3] cursor-pointer'>Decline</button>
               </div>
               <div>
-                <button className={`${isFollowed ? 'bg-[#CA3C3C] shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]' : 'bg-[#1565CE] shadow-[1px_1px_3px_0px_rgba(12,75,156,1)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)]'} px-2 sm:px-4 w-full h-fit rounded-2xl font-Roboto text-[#E3E3E3] transition-all  hover:opacity-90 cursor-pointer`} onClick={() => handleFollow(user.userId)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
+                <button className={`${isFollowed ? 'bg-[#CA3C3C] shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]' : 'bg-[#1565CE] shadow-[1px_1px_3px_0px_rgba(12,75,156,1)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)]'} px-2 sm:px-4 w-full h-fit rounded-2xl font-Roboto text-[#E3E3E3] transition-all  hover:opacity-90 cursor-pointer`} onClick={() => handleFollow(pathUser.userId)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
               </div>
             </div>
           ) : (
             <div className='w-full px-2 flex justify-between gap-4 pt-2 text-sm 2xl:text-base'>
               <button onClick={() => unfriend()} className='px-2 py-0 w-full h-fit rounded-full font-Roboto font-normal bg-[#CA3C3C] transition-all shadow-[1px_1px_3px_0px_rgba(202, 60, 60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)] hover:opacity-90 text-[#E3E3E3] cursor-pointer'>Unfriend</button>
-              <button className={`${isFollowed ? 'bg-[#CA3C3C] shadow-[1px_1px_3px_0px_rgba(202,60,60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]' : 'bg-[#1565CE] shadow-[1px_1px_3px_0px_rgba(12,75,156,1)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)]'} px-2 sm:px-4 w-full h-fit rounded-2xl font-Roboto text-[#E3E3E3] transition-all hover:opacity-90 cursor-pointer`} onClick={() => handleFollow(user.userId)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
+              <button className={`${isFollowed ? 'bg-[#CA3C3C] shadow-[1px_1px_3px_0px_rgba(202,60,60, 0.25)] hover:shadow-[1px_1px_5px_3px_rgba(202,60,60,0.4)]' : 'bg-[#1565CE] shadow-[1px_1px_3px_0px_rgba(12,75,156,1)] hover:shadow-[1px_1px_5px_3px_rgba(12,75,156,1)]'} px-2 sm:px-4 w-full h-fit rounded-2xl font-Roboto text-[#E3E3E3] transition-all hover:opacity-90 cursor-pointer`} onClick={() => handleFollow(pathUser.userId)}>{isFollowed ? 'Unfollow' : 'Follow'}</button>
             </div>
           )}
         </div>
@@ -260,7 +268,7 @@ const UserDetails = ({user, friendship, mutualFriends, popular}: {user: Profile,
               </div>    
             </div>
           )) : 
-          mutualFriends?.map((profile, index) => (
+          popularUsers?.map((profile, index) => (
             <div key={profile.userId} className="hover:cursor-pointer flex gap-2 py-2 items-center " onClick={() => router.push(`/users/${profile.username}`)}>
                 <Avatar className='w-[45px] h-[45px] 2xl:w-[45px] 2xl:h-[45px] 2k:w-[55px] 2k:h-[55px] rounded-full shadow-[0px_5px_5px_0px_rgba(0,_0,_0_,_0.25)]'>
                     <AvatarImage src={`${profile.pictureUrl}`} className="w-fit h-fit aspect-square rounded-full object-cover" /><AvatarFallback>{profile.username.slice(0, 1)}</AvatarFallback>
