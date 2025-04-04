9af69c7829d2a32a57c94496b77bc40a54bbc7b9
Marko Milković <mmilkovic2006@gmail.com>
2025-04-05 00:13:02 +0200

    Updating profile picture now allows images up to 5mb

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['snetblobstorage.blob.core.windows.net'], // Add the blob storage domain here
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb',
+    }
+  }
 };
 
 export default nextConfig;
